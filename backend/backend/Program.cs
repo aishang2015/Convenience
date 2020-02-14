e0c83de9b364bd1218daaf60ba26509e03351574
zhangtianhua <zth@outlook.com>
2020-02-14 11:42:10 +0800

    angular version update

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using backend.core.Data;
 using backend.data.Infrastructure;
+
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
 using NLog.Web;
 
 namespace backend
@@ -16,7 +13,7 @@ namespace backend
     {
         public static void Main(string[] args)
         {
-            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
             logger.Debug("init main");
             var host = CreateHostBuilder(args).Build();
             DataBaseInitializer.InitialDataBase<ApplicationDbContext>(host, ApplicationDbSeed.InitialApplicationDataBase);
