f3aecdcca59e9168a78ba77da5ae37272913bba9
Administrator <zhang__tian__hua@126.com>
2020-04-01 14:29:07 +0800

    add httpclient module

---
+++ src/Convience.FrameWork/Convience.Comunication/Convience.HttpClients/HttpClientExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Convience.HttpClients
+{
+    public static class HttpClientExtension
+    {
+        public static IServiceCollection AddHttpClients(this IServiceCollection services)
+        {
+            services.AddHttpClient();
+            return services;
+        }
+
+        public static IServiceCollection AddHttpClients<T>(this IServiceCollection services)
+            where T : AbstractHttpClient
+        {
+            services.AddHttpClient<T>();
+            return services;
+        }
+    }
+}
