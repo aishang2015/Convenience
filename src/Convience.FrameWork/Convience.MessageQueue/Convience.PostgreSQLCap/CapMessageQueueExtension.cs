7f89ef2378aef6459a745d321f1a26b509184bf6
Administrator <zhang__tian__hua@126.com>
2020-04-15 14:50:31 +0800

    add cap on postgresql

---
+++ src/Convience.FrameWork/Convience.MessageQueue/Convience.PostgreSQLCap/CapMessageQueueExtension.cs
@@ -0,0 +1,44 @@
+﻿using DotNetCore.CAP;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Convience.CapMQ
+{
+    public static class CapMessageQueueExtension
+    {
+        public enum CapMessageQueueType
+        {
+            RabbitMQ,
+            //Kafka,
+            //AzureServiceBus,
+        }
+
+        public static IServiceCollection AddPostgreCap<TDbContext>(this IServiceCollection services,
+            string dbConnectionString,
+            CapMessageQueueType messageQueueType,
+            Action<RabbitMQOptions> configure)
+            where TDbContext : DbContext
+        {
+            services.AddCap(o =>
+            {
+                o.UseEntityFramework<TDbContext>();
+
+                o.UsePostgreSql(dbConnectionString);
+
+                switch (messageQueueType)
+                {
+                    case CapMessageQueueType.RabbitMQ:
+                        o.UseRabbitMQ(configure);
+                        break;
+                }
+
+                o.UseDashboard();
+
+            });
+            return services;
+        }
+
+
+    }
+}
