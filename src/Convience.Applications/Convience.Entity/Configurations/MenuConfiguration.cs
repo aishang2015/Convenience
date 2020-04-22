245b9142406097f92c69ca97751737bf81609102
Administrator <zhang__tian__hua@126.com>
2020-04-22 16:42:48 +0800

    modify menu initialized data

--- src/Convience.Applications/Convience.Entity/Configurations/MenuConfiguration.cs
+++ src/Convience.Applications/Convience.Entity/Configurations/MenuConfiguration.cs
@@ -9,6 +9,9 @@ namespace Convience.Entity.Configurations
     {
         public void Configure(EntityTypeBuilder<Menu> builder)
         {
+            // int主键时在表中有初始化数据的情况下，需要指定开始的序列值。不进行设置的话，数据主键
+            // 会从1开始从而导致主键冲突。
+            builder.Property(m => m.Id).HasIdentityOptions(startValue: 48);
             builder.Property(m => m.Name).HasMaxLength(15);
             builder.Property(m => m.Identification).HasMaxLength(50);
             builder.Property(m => m.Permission).HasMaxLength(200);
---
+++ src/Convience.Applications/Convience.Entity/Configurations/MenuTreeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Convience.Entity.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Convience.Entity.Configurations
+{
+    public class MenuTreeConfiguration : IEntityTypeConfiguration<MenuTree>
+    {
+        public void Configure(EntityTypeBuilder<MenuTree> builder)
+        {
+            builder.Property(m => m.Id).HasIdentityOptions(startValue: 109);
+        }
+    }
+}
