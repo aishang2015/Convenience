c8b0f1ac9e4dfc2c2efe9996539b17b72250c428
Administrator <zhang__tian__hua@126.com>
2020-05-08 17:09:15 +0800

    add method of  handle dbcontext Concurrency save

--- src/Convience.FrameWork/Convience.Data/Convience.EntityFrameWork/Repositories/IUnitOfWork.cs
+++ src/Convience.FrameWork/Convience.Data/Convience.EntityFrameWork/Repositories/IUnitOfWork.cs
@@ -6,12 +6,34 @@ namespace Convience.EntityFrameWork.Repositories
 {
     public interface IUnitOfWork<TDbContext> where TDbContext : DbContext
     {
+        /// <summary>
+        /// 自动事务
+        /// </summary>
         Task SaveAsync();
 
+        /// <summary>
+        /// 并发保存（程序端优先）
+        /// </summary>
+        Task SaveByClientAsync(int retryCount = 3);
+
+        /// <summary>
+        /// 并发保存（合并程序端和数据库）
+        /// </summary>
+        Task SaveByMergeAsync(int retryCount = 3);
+
+        /// <summary>
+        /// 开启手动事务
+        /// </summary>
         Task StartTransactionAsync();
 
+        /// <summary>
+        /// 手动事务回滚
+        /// </summary>
         Task RollBackAsync();
 
+        /// <summary>
+        /// 手动事务提交
+        /// </summary>
         Task CommitAsync();
     }
 }
--- src/Convience.FrameWork/Convience.Data/Convience.EntityFrameWork/Repositories/UnitOfWork.cs
+++ src/Convience.FrameWork/Convience.Data/Convience.EntityFrameWork/Repositories/UnitOfWork.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
-
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Transactions;
 
@@ -20,6 +21,75 @@ namespace Convience.EntityFrameWork.Repositories
         public async Task SaveAsync()
         {
             await _dbContext.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// 客户端值优先，覆盖服务器值
+        /// </summary>
+        public async Task SaveByClientAsync(int retryCount = 3)
+        {
+            var isOk = false;
+            while (!isOk && retryCount-- > 0)
+            {
+                try
+                {
+                    await _dbContext.SaveChangesAsync();
+                    isOk = true;
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    foreach (var entity in ex.Entries)
+                    {
+                        await entity.ReloadAsync();
+                    }
+                    await _dbContext.SaveChangesAsync();
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 变更数据以数据库优先，未变更以客户端优先
+        /// </summary>
+        public async Task SaveByMergeAsync(int retryCount = 3)
+        {
+            var isOk = false;
+            while (!isOk && retryCount-- > 0)
+            {
+                try
+                {
+                    await _dbContext.SaveChangesAsync();
+                    isOk = true;
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    foreach (var entity in ex.Entries)
+                    {
+                        // 当前值
+                        var proposedValues = entity.CurrentValues;
+
+                        // 数据库最新值
+                        var databaseValues = entity.GetDatabaseValues();
+
+                        // 原始值
+                        var originalValues = entity.OriginalValues;
+
+                        entity.OriginalValues.SetValues(databaseValues);
+
+                        databaseValues.Properties
+                            .Where(property => !Equals(originalValues[property], databaseValues[property]))
+                            .ToList().ForEach(property => entity.Property(property.Name).IsModified = false);
+                    }
+                    await _dbContext.SaveChangesAsync();
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
         }
 
         public async Task StartTransactionAsync()
