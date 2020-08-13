28468dab0c21867794a906de077a3933ed2c563f
tenka <zhang__tian__hua@126.com>
2020-08-13 15:19:03 +0800

    change routing path to prevent the layout reload

--- src/Convience.Web/Managent/src/app/app-routing.module.ts
+++ src/Convience.Web/Managent/src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { LayoutComponent } from './pages/layout/layout/layout.component';
 
 const routes: Routes = [
   { path: "account", loadChildren: () => import("./pages/account/account.module").then(m => m.AccountModule) },
-  { path: "", component: LayoutComponent, loadChildren: () => import("./pages/infomation/infomation.module").then(m => m.InfomationModule) },
-  { path: "system", component: LayoutComponent, loadChildren: () => import("./pages/system-manage/system-manage.module").then(m => m.SystemManageModule) },
-  { path: "group", component: LayoutComponent, loadChildren: () => import("./pages/group-manage/group-manage.module").then(m => m.GroupManageModule) },
-  { path: "content", component: LayoutComponent, loadChildren: () => import("./pages/content-manage/content-manage.module").then(m => m.ContentManageModule) },
-  { path: "saas", component: LayoutComponent, loadChildren: () => import("./pages/saas-manage/saas-manage.module").then(m => m.SaasManageModule) },
-  { path: "tool", component: LayoutComponent, loadChildren: () => import("./pages/system-tool/system-tool.module").then(m => m.SystemToolModule) },
-  { path: "workflow", component: LayoutComponent, loadChildren: () => import("./pages/workflow/workflow.module").then(m => m.WorkflowModule) }
+  {
+    path: "", component: LayoutComponent, children: [
+      { path: "", loadChildren: () => import("./pages/infomation/infomation.module").then(m => m.InfomationModule) },
+      { path: "system", loadChildren: () => import("./pages/system-manage/system-manage.module").then(m => m.SystemManageModule) },
+      { path: "group", loadChildren: () => import("./pages/group-manage/group-manage.module").then(m => m.GroupManageModule) },
+      { path: "content", loadChildren: () => import("./pages/content-manage/content-manage.module").then(m => m.ContentManageModule) },
+      { path: "saas", loadChildren: () => import("./pages/saas-manage/saas-manage.module").then(m => m.SaasManageModule) },
+      { path: "tool", loadChildren: () => import("./pages/system-tool/system-tool.module").then(m => m.SystemToolModule) },
+      { path: "workflow", loadChildren: () => import("./pages/workflow/workflow.module").then(m => m.WorkflowModule) }
+    ]
+  },
 ];
 
 @NgModule({
