1bdac04f5c6fdba5f029929de8ac6cd389b51810
tenka <zhang__tian__hua@126.com>
2020-08-17 13:33:51 +0800

    add dbconnection modity method

--- src/Convience.Backend/Convience.FrameWork/Convience.Data/Convience.EntityFrameWork/Saas/SaasExtension.cs
+++ src/Convience.Backend/Convience.FrameWork/Convience.Data/Convience.EntityFrameWork/Saas/SaasExtension.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Convience.EntityFrameWork.Saas
 {
     public static class SaasExtension
     {
+        /// <summary>
+        /// 添加服务
+        /// </summary>
         public static IServiceCollection AddSchemaService(this IServiceCollection services)
         {
             services.AddScoped<ISchemaService, SchemaService>();
             return services;
+        }
+
+        /// <summary>
+        /// 修改数据库连接串
+        /// </summary>
+        public static void ChangeDbContextConnection(this DbContext dbContext, string targetDbConnection)
+        {
+            dbContext.Database.GetDbConnection().ConnectionString = targetDbConnection;
         }
     }
 }
