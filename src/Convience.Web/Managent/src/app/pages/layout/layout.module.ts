0f6846dca1690a75cb967297814b16beae3b429e
zhangtianhua <zth@outlook.com>
2020-10-01 09:32:24 +0800

    add menu

--- src/Convience.Web/Managent/src/app/pages/layout/layout.module.ts
+++ src/Convience.Web/Managent/src/app/pages/layout/layout.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
-import { NzBreadCrumbModule, NzLayoutModule, NzDropDownModule, NzCardModule, NzAvatarModule, NzBadgeModule, NzIconModule, NzMessageModule, NzFormModule, NzButtonComponent, NzButtonModule, NzInputModule, NzModalModule } from 'ng-zorro-antd';
 import { RouterModule } from '@angular/router';
 import { AppCommonModule } from '../app-common/app-common.module';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzTagModule } from 'ng-zorro-antd/tag';
 
 
 
@@ -28,6 +40,7 @@ import { AppCommonModule } from '../app-common/app-common.module';
     NzButtonModule,
     NzInputModule,
     NzModalModule,
+    NzTagModule,
   ],
   exports: [
     LayoutComponent
