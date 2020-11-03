403014bac0c1ed1d79b4beae3a2e35bcd679cbc5
tenka <zhang__tian__hua@126.com>
2020-11-03 14:47:57 +0800

    add base service and clean code

---
+++ src/Convience.Backend/Convience.Applications/Convience.Service/BaseService.cs
@@ -0,0 +1,73 @@
+﻿using Convience.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Convience.Service
+{
+    public interface IBaseService
+    {
+        public PagingResultModel<T> GetPagingResult<T>(IQueryable<T> query, int page, int size) where T : class;
+
+        public IQueryable<T> GetOrderQuery<T>(IQueryable<T> query, string sort, string order,
+            Dictionary<string, Expression<Func<T, object>>> properties);
+    }
+
+    public class BaseService : IBaseService
+    {
+
+        public PagingResultModel<T> GetPagingResult<T>(IQueryable<T> query, int page, int size) where T : class
+        {
+            var skip = size * (page - 1);
+            return new PagingResultModel<T>()
+            {
+                Data = query.Skip(skip).Take(size).ToList(),
+                Count = query.Count()
+            };
+        }
+
+
+        /// <summary>
+        /// 设置数据排序方式
+        /// </summary>
+        public IQueryable<T> GetOrderQuery<T>(IQueryable<T> query, string sort, string order,
+            Dictionary<string, Expression<Func<T, object>>> properties)
+        {
+            if (!string.IsNullOrEmpty(sort) && !string.IsNullOrEmpty(order))
+            {
+                var isOrderQuaryable = false;
+                var sortArray = sort.Split(',', StringSplitOptions.RemoveEmptyEntries);
+                var orderArray = order.Split(',', StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < sortArray.Length; i++)
+                {
+                    var field = sortArray[i];
+                    var orderByExpression = properties[field];
+
+                    // 没有相应的字段退出
+                    if (orderByExpression == null)
+                    {
+                        continue;
+                    }
+                    var o = orderArray[i];
+
+                    // 添加排序
+                    if (isOrderQuaryable)
+                    {
+                        query = o == "ascend" ?
+                            (query as IOrderedQueryable<T>).ThenBy(orderByExpression) :
+                            (query as IOrderedQueryable<T>).ThenByDescending(orderByExpression);
+                    }
+                    else
+                    {
+                        isOrderQuaryable = true;
+                        query = o == "ascend" ?
+                            query.OrderBy(orderByExpression) :
+                            query.OrderByDescending(orderByExpression);
+                    }
+                }
+            }
+            return query;
+        }
+    }
+}
--- src/Convience.Backend/Convience.FrameWork/Convience.Taskscheduling/Convience.Hangfire/HangFireExtension.cs
+++ src/Convience.Backend/Convience.FrameWork/Convience.Taskscheduling/Convience.Hangfire/HangFireExtension.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 using System;
-using System.Transactions;
 
 namespace Convience.Hangfire
 {
