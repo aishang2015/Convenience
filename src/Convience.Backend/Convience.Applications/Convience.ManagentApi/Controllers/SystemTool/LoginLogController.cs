4fad5e899cd7940a9cb7cdbc5ca44df4b6c6d3de
zhangtianhua <zth@outlook.com>
2021-02-15 14:18:23 +0800

    add login log controller

---
+++ src/Convience.Backend/Convience.Applications/Convience.ManagentApi/Controllers/SystemTool/LoginLogController.cs
@@ -0,0 +1,53 @@
+﻿using Convience.Model.Models.SystemTool;
+using Convience.Service.SystemTool;
+using Convience.Util.Extension;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Convience.ManagentApi.Controllers.SystemTool
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginLogController : ControllerBase
+    {
+        public readonly ILoginLogService _loginLogService;
+
+        public LoginLogController(ILoginLogService loginLogService)
+        {
+            _loginLogService = loginLogService;
+        }
+
+        [HttpGet("setting")]
+        public IActionResult GetSetting()
+        {
+            var result = _loginLogService.GetSetting();
+            return Ok(result);
+        }
+
+        [HttpPost("setting")]
+        public async Task<IActionResult> UpdateSetting([FromBody] LoginLogSettingViewModel viewModel)
+        {
+            var isSuccess = await _loginLogService.UpdateLoginLogSettingAsync(viewModel);
+            if (!isSuccess)
+            {
+                return this.BadRequestResult("更新失败！");
+            }
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetLoginLog([FromQuery] LoginLogQueryModel queryModel)
+        {
+            var resultModel = _loginLogService.GetLoginLogDetail(queryModel);
+            return Ok(resultModel);
+        }
+    }
+
+
+}
---
+++ src/Convience.Backend/Convience.Applications/Convience.Model/Models/SystemTool/LoginLogModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Convience.Model.Models.SystemTool
+{
+    public class LoginLogSettingViewModel
+    {
+        public int SaveTime { get; set; }
+    }
+
+    public class LoginLogSettingResultModel : LoginLogSettingViewModel { }
+
+    public class LoginLogQueryModel : PageSortQueryModel
+    {
+        public string Account { get; set; }
+    }
+
+    public class LoginLogDetailResultModel
+    {
+
+        public string OperatorAccount { get; set; }
+
+        public DateTime OperateAt { get; set; }
+
+        public string IpAddress { get; set; }
+
+        public bool IsSuccess { get; set; }
+    }
+
+}
